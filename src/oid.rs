43fc66926e2069a57724185076a2334ffa3e053f
Martin Hoffmann <martin@nlnetlabs.nl>
2026-07-13 11:42:10 +0200

    Fix panic in Oid::from_str with large second component.

--- src/oid.rs
+++ src/oid.rs
@@ -269,7 +269,11 @@ impl<T: AsRef<[u8]> + From<Vec<u8>>> FromStr for Oid<T> {
             return Err("second component for 0. and 1. must be less than 40");
         }
 
-        let mut res = vec![40 * first + second];
+        let mut res = vec![
+            first.checked_mul(40).and_then(|first| {
+                first.checked_add(second)
+            }).ok_or("overflow in second component")?
+        ];
         for item in components {
             res.push(from_str(item)?);
         }
@@ -481,6 +485,15 @@ impl<'a> Iterator for Iter<'a> {
 #[cfg(test)]
 mod test {
     use super::*;
+
+    #[test]
+    fn from_str() {
+        assert_eq!(
+            Oid::<Vec<u8>>::from_str("2.5.29.19").unwrap(),
+            Oid(&[85, 29, 19])
+        );
+        assert!(Oid::<Vec<u8>>::from_str("2.4294967295").is_err());
+    }
 
     #[test]
     fn display() {
